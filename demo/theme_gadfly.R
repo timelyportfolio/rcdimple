3d058cff6a7078997fd96867dde8c827b9e70f4f
timelyportfolio <kent.russell@timelyportfolio.com>
2015-08-22 05:48:08 -0500

    separate grid and axis line styling; make axis dash-array smaller

--- demo/theme_gadfly.R
+++ demo/theme_gadfly.R
@@ -26,37 +26,40 @@ data.frame(x = 1:10, y = 1:10, groups = letters[round(runif(10,1,5))]) %>>%
 #   this effectively namespaces the styling to the chart
 #   also with an array of objects this could be made extensible
 #   for other themes
-d1$x$options$tasks = list(htmlwidgets::JS(
-'
-function(){
+task <- list(htmlwidgets::JS(
+  '
+  function(){
   var chart = d3.select(this);
-
+  
   chart.selectAll( ".dimple-custom-axis-label" )
-    .style("font-size", "1.15em")
-    .style("color", "#564a55");
+  .style("font-size", "1.15em")
+  .style("color", "#564a55");
   chart.selectAll(".dimple-custom-axis-title")
-    .style("font-size", "0.95em")
-    .style("color","#564a55");
+  .style("font-size", "0.95em")
+  .style("color","#564a55");
   // tooltip does not work since gets added on the fly
   //  how to handle
   chart.selectAll(".dimple-custom-tooltip-box")
-    .style("stroke","none")
-    .style("opacity","0.8");
+  .style("stroke","none")
+  .style("opacity","0.8");
   chart.selectAll(".dimple-custom-tooltip-label")
-    .style("font-size", "0.9em");
+  .style("font-size", "0.9em");
   chart.selectAll(".dimple-custom-series-bubble")
-    .style("stroke","none");
+  .style("stroke","none");
   chart.selectAll(".dimple-custom-series-line")
-    .style("stroke-width", "0.3mm");
-  chart.selectAll(".dimple-custom-gridline, .dimple-custom-axis-line")
-    .style("stroke", "#D0D0E0")
-    .style("stroke-width", "0.08em")
-    .style("fill", "none");
+  .style("stroke-width", "0.3mm");
+  chart.selectAll(".dimple-custom-axis-line")
+  .style("stroke", "#D0D0E0")
+  .style("stroke-width", "0.09em")
+  .style("fill", "none");
   chart.selectAll(".dimple-custom-gridline")
-    .style("stroke-dasharray", "0.2em, 0.2em");
-}
-'
+  .style("stroke", "#D0D0E0")
+  .style("stroke-width", "0.07em")
+  .style("stroke-dasharray", "0.15em, 0.15em");
+  }
+  '
 ))
+d1$x$options$tasks = task
 
 d1
 
